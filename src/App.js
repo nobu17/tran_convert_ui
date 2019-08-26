3df43e5fb27ea3b441e7bb5506cfcde7fe8b9046
nobu17 <lunacy171710@yahoo.co.jp>
2019-08-27 08:58:53 +0900

    add first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import TranInput from "./tranInput";
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <TranInput />
       </header>
     </div>
   );
---
+++ src/tranOutput.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import TextareaAutosize from "@material-ui/core/TextareaAutosize";
+
+class TranOutput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rawTran: "bbbbb",
+      convertStr: ""
+    };
+    this.onRawTranChange = this.onRawTranChange.bind(this);
+  }
+  onRawTranChange(e) {
+    this.setState({ rawTran: e.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <Grid container>
+          <Grid item xs={12}>
+            <TextareaAutosize
+              rows={4}
+              placeholder="input tran"
+              value={this.state.rawTran}
+              onChange={this.onRawTranChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary">
+              登録
+            </Button>
+          </Grid>
+        </Grid>
+        <table></table>
+      </div>
+    );
+  }
+}
+
+export default TranOutput;
