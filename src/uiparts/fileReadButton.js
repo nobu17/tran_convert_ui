6c8f72583270e08a2595b21ffe50a79443f1eedc
inagawa <lunacy171710@yahoo.co.jp>
2019-11-28 16:24:09 +0900

    change fileread encode

--- src/uiparts/fileReadButton.js
+++ src/uiparts/fileReadButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
+import Encoding from "encoding-japanese";
 
 const MaxSize = 2000000;
 
@@ -30,13 +31,31 @@ export default class FileReadButton extends React.Component {
       const reader = new FileReader();
       const props = this.props;
       reader.onload = function() {
-        const text = reader.result;
+        // const text = reader.result;
+        // 8ビット符号なし整数値配列と見なす
+        let array = new Uint8Array(reader.result);
+        // 文字コードを取得
+        switch (Encoding.detect(array)) {
+          case "UTF16":
+            // 16ビット符号なし整数値配列と見なす
+            array = new Uint16Array(e.target.result);
+            break;
+          case "UTF32":
+            // 32ビット符号なし整数値配列と見なす
+            array = new Uint32Array(e.target.result);
+            break;
+        }
+
+        // Unicodeの数値配列に変換
+        const unicodeArray = Encoding.convert(array, "UNICODE");
+        // Unicodeの数値配列を文字列に変換
+        const text = Encoding.codeToString(unicodeArray);
         props.fileRead(text);
       };
       reader.onerror = function(err) {
         props.fileReadFailed(err);
       };
-      reader.readAsText(input.files[0]);
+      reader.readAsArrayBuffer(input.files[0]);
     }
   }
   render() {
